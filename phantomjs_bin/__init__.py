fdc8cb1be954a31f3f7fd6d34acaa6ba38144034
jayjhwu <jayjhwu@tencent.com>
2018-11-28 15:55:05 +0800

    clean file

--- phantomjs_bin/__init__.py
+++ phantomjs_bin/__init__.py
@@ -1,45 +1,18 @@
 import os
 import platform
 
-BINARY_VERSION = '2.1.1'
-
 
 def _get_executable_path():
-
-    os_type_mapping = {
-        "Windows": {
-            "dir": "windows",
-            "files": {
-                "32bit": "phantomjs.exe",
-                "64bit": "phantomjs.exe",
-            }
-        },
-        "Darwin": {
-            "dir": "macosx",
-            "files": {
-                "32bit": "phantomjs",
-                "64bit": "phantomjs",
-            }
-        },
-        "Linux": {
-            "dir": "linux",
-            "files": {
-                "32bit": "phantomjs-i686",
-                "64bit": "phantomjs-x86_64",
-            }
-        },
-    }
-
-    bin_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin", BINARY_VERSION)
+    bin_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin")
 
     os_type = platform.system()
-    os_bits = platform.architecture()[0]
 
-    if os_type in os_type_mapping:
-        os_info = os_type_mapping[os_type]
-        return os.path.join(bin_dir, os_info["dir"], os_info["files"].get(os_bits, os_info["files"]["64bit"]))
+    if os_type == 'Windows':
+        bin_filename = "phantomjs.exe"
+    else:
+        bin_filename = "phantomjs"
 
-    raise EnvironmentError("Unsupported system type: %s" % os_type)
+    return os.path.join(bin_dir, bin_filename)
 
 
 executable_path = _get_executable_path()
--- setup.py
+++ setup.py
@@ -1,19 +1,80 @@
 #!/usr/bin/env python
+import os
+import platform
+import shutil
 
 import setuptools
+from setuptools.command.install import install as install_base
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class install(install_base):
+    def run(self):
+        os_type_mapping = {
+            "Windows": {
+                "dir": "windows",
+                "files": {
+                    "32bit": "phantomjs.exe",
+                    "64bit": "phantomjs.exe",
+                }
+            },
+            "Darwin": {
+                "dir": "macosx",
+                "files": {
+                    "32bit": "phantomjs",
+                    "64bit": "phantomjs",
+                }
+            },
+            "Linux": {
+                "dir": "linux",
+                "files": {
+                    "32bit": "phantomjs-i686",
+                    "64bit": "phantomjs-x86_64",
+                }
+            },
+        }
+
+        setup_path = os.path.dirname(os.path.abspath(__file__))
+        bin_dir = os.path.join(setup_path, "bin")
+        install_dir = os.path.join(setup_path, "phantomjs_bin", "bin")
+
+        os_type = platform.system()
+        os_bits = platform.architecture()[0]
+
+        if os_type not in os_type_mapping:
+            raise EnvironmentError("Unsupported system type: %s" % os_type)
+
+        os_info = os_type_mapping[os_type]
+        bin_path = os.path.join(bin_dir, os_info["dir"], os_info["files"].get(os_bits, os_info["files"]["64bit"]))
+
+        print "Installing phantomjs-2.1.1 for %s..." % os_type
+
+        if not os.path.exists(install_dir):
+            os.mkdir(install_dir)
+
+        if os_type == 'Windows':
+            installed_bin_filename = "phantomjs.exe"
+        else:
+            installed_bin_filename = "phantomjs"
+
+        # move bin file
+        shutil.copyfile(bin_path, os.path.join(install_dir, installed_bin_filename))
+
+        install_base.run(self)
+
+
 setuptools.setup(
-    name='PhantomJS Binary',
+    name='phantomjs_binary',
     version='1.0',
     description='A pip package for installing binary files of phantomjs',
     long_description=long_description,
-    long_description_content_type="text/markdown",
     author='jayjiahua',
     author_email='jayjhwu@gmail.com',
     url='https://github.com/jayjiahua/phantomjs-bin-pip',
     packages=['phantomjs_bin'],
     include_package_data=True,
+    cmdclass={'install': install},
 )
