f827527c1f9908744674bc0217975fd6f75c0e36
jayjhwu(吴嘉华) <jayjhwu@tencent.com>
2018-12-09 10:24:35 +0800

    minor: update version num

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class install(install_base):
 
 setuptools.setup(
     name='phantomjs-binary',
-    version='1.0',
+    version='2.1.1',
     description='A pip package for installing binary files of phantomjs',
     long_description=long_description,
     author='jayjiahua',
