a2073b25415983b49ad63fd4907a85525fe9f5b2
jayjhwu(吴嘉华) <jayjhwu@tencent.com>
2018-12-11 15:42:25 +0800

    update bin path

--- phantomjs_bin/__init__.py
+++ phantomjs_bin/__init__.py
@@ -3,16 +3,34 @@ import platform
 
 
 def _get_executable_path():
-    bin_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin")
+    os_type_mapping = {
+        "Windows": {
+            "dir": "windows",
+        },
+        "Darwin": {
+            "dir": "macosx",
+        },
+        "Linux": {
+            "dir": "linux",
+        },
+    }
 
     os_type = platform.system()
+
+    if os_type not in os_type_mapping:
+        raise EnvironmentError("Unsupported system type: %s" % os_type)
+
+    os_info = os_type_mapping[os_type]
 
     if os_type == 'Windows':
         bin_filename = "phantomjs.exe"
     else:
         bin_filename = "phantomjs"
 
-    return os.path.join(bin_dir, bin_filename)
+    setup_path = os.path.dirname(os.path.abspath(__file__))
+    bin_path = os.path.join(setup_path, 'bin', os_info["dir"], bin_filename)
+
+    return bin_path
 
 
 executable_path = _get_executable_path()
--- setup.py
+++ setup.py
@@ -1,66 +1,14 @@
 #!/usr/bin/env python
-import os
-import platform
-import shutil
-import stat
-from glob import glob
 import setuptools
-from setuptools.command.install import install as install_base
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-class install(install_base):
-    def run(self):
-        os_type_mapping = {
-            "Windows": {
-                "dir": "windows",
-            },
-            "Darwin": {
-                "dir": "macosx",
-            },
-            "Linux": {
-                "dir": "linux",
-            },
-        }
-
-        setup_path = os.path.dirname(os.path.abspath(__file__))
-        bin_dir = os.path.join(setup_path, "bin")
-        install_dir = os.path.join(setup_path, "phantomjs_bin", "bin")
-
-        os_type = platform.system()
-        os_bits = platform.architecture()[0]
-
-        if os_type not in os_type_mapping:
-            raise EnvironmentError("Unsupported system type: %s" % os_type)
-
-        if os_type == 'Windows':
-            installed_bin_filename = "phantomjs.exe"
-        else:
-            installed_bin_filename = "phantomjs"
-
-        os_info = os_type_mapping[os_type]
-        bin_path = os.path.join(bin_dir, os_info["dir"], installed_bin_filename)
-
-        if not os.path.exists(install_dir):
-            os.mkdir(install_dir)
-
-        install_path = os.path.join(install_dir, installed_bin_filename)
-        # move bin file
-        shutil.copyfile(bin_path, install_path)
-
-        # make executable
-        st = os.stat(install_path)
-        os.chmod(install_path, st.st_mode | stat.S_IEXEC)
-
-        install_base.run(self)
-
-
 setuptools.setup(
     name='phantomjs-binary',
-    version='2.1.2',
+    version='2.1.3',
     description='A pip package for installing binary files of phantomjs',
     long_description=long_description,
     author='jayjiahua',
@@ -68,6 +16,4 @@ setuptools.setup(
     url='https://github.com/jayjiahua/phantomjs-bin-pip',
     packages=['phantomjs_bin'],
     include_package_data=True,
-    cmdclass={'install': install},
-    package_data={'bin': glob('*/*')},
 )
